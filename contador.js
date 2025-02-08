08bf99cf3c4e0eabef099a37868604acb8fbb7cd
JuanRdz <juan.rodriguezlr@uanl.edu.mx>
2025-02-08 00:56:13 -0600

    CAMBIOS A COMO ESTA LA PAGINA ACTUAL

--- contador.js
+++ contador.js
@@ -1,5 +1,42 @@
 let contador = 0; //Let se comporta como una variable que es mas estricta y solo funciona si se le asigna un valor.
 
+const BotonOscuro=document.getElementById('Modo-Oscuro');
+
+CargarModo();
+
+function ActivarModo()
+{
+    const body=document.body;
+
+    body.classList.toggle('modo-oscuro');
+
+    if(body.classList.contains('modo-oscuro'))
+    {
+        localStorage.setItem('modo', 'oscuro');
+        BotonOscuro.value = 'Modo Claro';
+    }
+    else
+    {
+        localStorage.setItem('modo', 'claro');
+        BotonOscuro.value='Modo Oscuro';
+    }
+}
+
+function CargarModo()
+{
+    const modo = localStorage.getItem('modo');
+    if(modo === 'oscuro')
+    {
+        document.body.classList.add('modo-oscuro');
+        BotonOscuro.value='Modo Claro';
+    }
+
+}
+
+BotonOscuro.addEventListener('click', ActivarModo);
+
+
+
 document.getElementById("boton").addEventListener("click", function()
 {
     contador++;
